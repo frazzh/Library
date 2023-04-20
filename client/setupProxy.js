3c6082dae6eb15268f806e759858b8e508c7b951
Hasan Faraz <121616642+frazzh@users.noreply.github.com>
2023-04-20 11:05:13 +0530

    Update setupProxy.js

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function (app) {
     app.use(proxy('/api',
         {
-            target: 'http://54.227.162.108:5000/',
+            target: 'http://54.175.192.150:5000/',
             secure: false
         }
     ));
